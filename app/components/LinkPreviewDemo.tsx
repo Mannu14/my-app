e1adc44b3f42669a21c07e4a2dc948fb06464bae
Mannu14 <yadumannu538@gmail.com>
2024-11-03 06:32:15 +0530

    change name

--- app/components/LinkPreviewDemo.tsx
+++ app/components/LinkPreviewDemo.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import React from "react";
+import Link from 'next/link';
 import { LinkPreview } from "./ui/link-preview";
 interface LinkPreviewDemoProps {
   url: string;
@@ -12,7 +13,9 @@ export function LinkPreviewDemo({ Name, url }: LinkPreviewDemoProps) {
     <div className="flex justify-center items-center h-[40rem] flex-col px-4">
       <p className="text-neutral-font text-neutral-500 dark:text-neutral-400 text-xl md:text-3xl max-w-3xl mx-auto mb-10">
         <LinkPreview url={url} className="font-bold">
-          {Name}
+          <Link href={url} passHref>
+            <a target="_blank" rel="noopener noreferrer">{Name}</a>
+          </Link>
         </LinkPreview>
       </p>
     </div>
