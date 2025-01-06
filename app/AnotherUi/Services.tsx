56c06ef9809d5de35eaa331a38a3e51f4f39d1ab
Mannu14 <yadumannu538@gmail.com>
2025-01-06 16:01:10 +0530

    update

--- app/AnotherUi/Services.tsx
+++ app/AnotherUi/Services.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import { Code2, BookOpen, Briefcase } from "lucide-react";
 import { Star, StarHalf } from "lucide-react";
 
-const ServiceCard = ({ icon: Icon, title, description, link, images }: { icon: React.ComponentType<any>; title: string; description: string; link?: string, images?: string }) => {
+const ServiceCard = ({ icon: Icon, title, description, link, images }: { icon: any; title: string; description: string; link?: string, images?: string }) => {
   // Determine the star fill logic based on the title
   let filledStars = 0;
   if (title === "Full Stack Developer") {
